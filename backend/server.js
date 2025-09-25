47a0105b91121ee0632bf273191263ca7401f3a1
Shivi Agawal <shiviagarwal253@gmail.com>
2025-09-25 21:44:21 +0530

    fixed backend server

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+// backend/server.js
+
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db.js');
@@ -23,7 +25,7 @@ const server = app.listen(PORT, () => {
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: 'http://localhost:3000',
+        origin: process.env.FRONTEND_URL || "http://localhost:3000"
     }
 });
 
